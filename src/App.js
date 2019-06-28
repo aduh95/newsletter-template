c592169f72c5ce9098771cacf49164c0e507179d
Antoine du HAMEL <duhamelantoine1995@gmail.com>
2019-06-28 11:30:21 +0200

    Add drag&droping of files

--- src/App.js
+++ src/App.js
@@ -29,16 +29,53 @@ export default class App extends Component {
   componentWillMount() {
     const savedContent = localStorage.getItem(CONTENT_KEY);
 
-    const content = savedContent
-      ? this.deserialize(JSON.parse(savedContent))
-      : this.state.content;
+    let { content } = this.state;
+
+    try {
+      content = this.deserialize(JSON.parse(savedContent));
+    } catch (e) {
+      console.warn(e);
+    }
+
     this.setState({ ...this.state, content });
   }
 
-  componentDidUpdate() {}
+  componentDidMount() {
+    const DRAG_CLASS_NAME = "dragover";
+    document.body.addEventListener("drop", e => {
+      e.preventDefault();
+      document.documentElement.classList.remove(DRAG_CLASS_NAME);
 
-  serialize(array, i, data) {
-    array[i] = data;
+      for (let file of e.dataTransfer.items || e.dataTransfer.files) {
+        if (file.kind === "file") {
+          file = file.getAsFile();
+        }
+        console.log(file);
+
+        file
+          .text()
+          .then(JSON.parse)
+          .then(data => {
+            this.setState({ ...this.state, content: this.deserialize(data) });
+            this.serialize();
+          })
+          .catch(console.error);
+      }
+    });
+    document.body.addEventListener("dragover", e => {
+      e.preventDefault();
+      document.documentElement.classList.add(DRAG_CLASS_NAME);
+    });
+    document.body.addEventListener("dragleave", e => {
+      e.preventDefault();
+      document.documentElement.classList.remove(DRAG_CLASS_NAME);
+    });
+  }
+
+  serialize(array = null, i = null, data = null) {
+    if (Array.isArray(array) && Number.isInteger(i)) {
+      array[i] = data;
+    }
     localStorage.setItem(CONTENT_KEY, JSON.stringify(CONTENT_CACHE));
   }
 
