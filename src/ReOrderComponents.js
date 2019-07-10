c9b2cbe379c56e3e28a912f1f92fba16731b321d
Antoine du HAMEL <duhamelantoine1995@gmail.com>
2019-07-10 11:40:26 -0400

    Handle element reordering (DOM only)

--- src/ReOrderComponents.js
+++ src/ReOrderComponents.js
@@ -7,6 +7,9 @@ const DROP_ZONE_ID = "re-order-elements";
 const CHECK_CHILDREN_ELEMENT = "re-order-elements-check-children";
 const RE_ORDERING_CLASS_NAME = "re-order-elements-reordering";
 
+/**
+ * @type {HTMLElement[]}
+ */
 const observees = [];
 
 const clickHandler = e => {
@@ -16,10 +19,12 @@ const clickHandler = e => {
   const subElements = target.querySelectorAll(
     `#${CHECK_CHILDREN_ELEMENT}>[data-name]`
   );
+  console.log("click", e);
   console.log(target, subElements);
 
   if (subElements.length) {
     try {
+      // TODO replace with ES next ?. syntax
       document.getElementById(RE_ORDERING_CLASS_NAME).removeAttribute("id");
     } catch {}
 
@@ -29,30 +34,78 @@ const clickHandler = e => {
     flushEventListeners();
     Array.from(subElements).forEach(addEventListener);
 
-    target.scrollIntoViewIfNeeded();
+    if (subElements.length === 1) {
+      // If there's just 1 Element, try to travers as there's nothing to reorder
+      subElements[0].click();
+    } else {
+      target.scrollIntoViewIfNeeded();
+    }
   } else {
     target.removeAttribute("id");
   }
 };
 
-const dragHandler = e => {
+/**
+ * @param {DragEvent} e
+ */
+const dragStartHandler = e => {
   // TODO
-  new IntersectionObserver(console.log);
+  const { target } = e;
+  const { parentElement } = target;
+  let destination;
+
+  /**
+   * @param {DragEvent} e
+   */
+  const dragOverHandler = e => {
+    e.stopImmediatePropagation();
+    destination = e.toElement;
+  };
+  const dragLeaveHandler = () => {
+    destination = null;
+  };
+
+  parentElement.addEventListener("dragover", dragOverHandler, {
+    passive: true,
+  });
+  document.body.addEventListener("dragover", dragLeaveHandler, {
+    passive: true,
+  });
+  target.addEventListener(
+    "dragend",
+    () => {
+      if (destination) {
+        parentElement.insertBefore(target, destination);
+      } else {
+        target.remove();
+      }
+      parentElement.removeEventListener("dragover", dragOverHandler);
+      document.body.removeEventListener("dragover", dragLeaveHandler);
+    },
+    {
+      once: true,
+      passive: true,
+    }
+  );
 };
 
+/**
+ * @param {HTMLElement} el
+ */
 const addEventListener = el => {
   observees.push(el);
   el.addEventListener("click", clickHandler);
-  el.addEventListener("drag", dragHandler);
+  el.addEventListener("dragstart", dragStartHandler);
   el.draggable = true;
 };
 
 const flushEventListeners = () => {
   observees.forEach(el => {
     el.removeEventListener("click", clickHandler);
-    el.removeEventListener("drag", dragHandler);
+    el.removeEventListener("dragstart", dragStartHandler);
     el.draggable = false;
   });
+  observees.splice(0);
 };
 
 export default class ReOrderComponents extends Component {
