85f2f51c2f7dbc769e6472fffe1245250838280d
Antoine du HAMEL <duhamelantoine1995@gmail.com>
2019-07-24 20:15:02 -0400

    Export JSON file

--- src/Save.js
+++ src/Save.js
@@ -1,7 +1,62 @@
 import { h, Component, Fragment } from "preact";
 
-export default class Save extends Component {
+if (!("requestIdleCallback" in window)) {
+  import("./polyfill/window-requestIdleCallback.js");
+}
+
+class MultiChoiceMenu extends Component {
   render() {
-    return <>Save</>;
+    return (
+      <form>
+        <p>{this.props.childen.map(child => child)}</p>
+      </form>
+    );
+  }
+}
+
+class Option extends Component {
+  render() {
+    return (
+      <label>
+        <input type="radio" name={this.props.name} />
+        {this.props.label}
+      </label>
+    );
+  }
+}
+
+export default class Save extends Component {
+  exportHTML(node) {
+    node = node.cloneNode(true);
+    Array.from(
+      node.querySelectorAll("output[hidden]"),
+      HTMLElement.prototype.remove
+    );
+    ["json", "ignore", "key"].forEach(key =>
+      Array.from(node.querySelectorAll(`[data-${key}]`), el =>
+        el.dataset.removeDataAttr(key)
+      )
+    );
+
+    return node.outerHTML;
+  }
+
+  clickHandler() {
+    const fileName = "editorialCalendar.json";
+    const a = document.createElement("a");
+    const file = new File([JSON.stringify(this.props.editor.data)], fileName, {
+      type: "application/json",
+    });
+    a.href = URL.createObjectURL(file);
+    a.download = fileName;
+    document.body.append(a);
+    a.click();
+    requestIdleCallback(() => {
+      URL.revokeObjectURL(a.href);
+      a.remove();
+    });
+  }
+  render() {
+    return <button onClick={() => this.clickHandler()}>Download JSON</button>;
   }
 }
