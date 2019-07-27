75043c2f7d3f0ac497ec72c90d857eb2fcec47aa
Antoine du HAMEL <duhamelantoine1995@gmail.com>
2019-07-27 16:12:19 -0400

    Ignore invalid components

--- src/App.js
+++ src/App.js
@@ -30,20 +30,24 @@ export default class App extends Component {
   }
 
   getComponents(array) {
-    return array.map((props, i, array) => {
-      const { type } = props;
-      if (!ASYNC_COMP.has(type)) {
-        ASYNC_COMP.set(
-          type,
-          lazy(() =>
-            /[^\w]/.test(type)
-              ? Promise.reject(new Error("Invalid component name"))
-              : import(`./components/${type}.js`)
-          )
-        );
+    return array.map((props, i) => {
+      try {
+        const { type } = props;
+        if (!ASYNC_COMP.has(type)) {
+          ASYNC_COMP.set(
+            type,
+            lazy(() =>
+              /[^\w]/.test(type)
+                ? Promise.reject(new Error("Invalid component name"))
+                : import(`./components/${type}.js`)
+            )
+          );
+        }
+        const Component = ASYNC_COMP.get(type);
+        return <Component key={i} {...props} />;
+      } catch {
+        return <div data-ignore>Invalid component</div>;
       }
-      const Component = ASYNC_COMP.get(type);
-      return <Component key={i} {...props} />;
     });
   }
 
