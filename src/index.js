9df5ef8061d707b6f2f5cb24ea8811e4c3d33011
Antoine du HAMEL <duhamelantoine1995@gmail.com>
2019-08-08 23:00:25 -0400

    Add fallback message for unsupported browsers

--- src/index.js
+++ src/index.js
@@ -5,6 +5,11 @@ import "./modifier.scss";
 import "./newsletter.css";
 import App from "./App.js";
 
+// Disable fallback-message as the browser supports the app
+// (browsers that don't won't be able to parse the script anyway)
+document.getElementById("fallback-message").remove();
+document.body.removeAttribute("onload");
+
 render(<App />, document.body);
 
 import "./polyfill/bootstrap.js";
