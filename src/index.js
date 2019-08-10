b5fd7951f16c744c5a869f43452544c9634d4e22
Antoine du HAMEL <duhamelantoine1995@gmail.com>
2019-08-10 12:12:53 -0400

    Add delay before displaying fallback message
    This is to avoid flashing an error message on supported browsers

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ import App from "./App.js";
 // Disable fallback-message as the browser supports the app
 // (browsers that don't won't be able to parse the script anyway)
 document.getElementById("fallback-message").remove();
-document.body.removeAttribute("onload");
 
 render(<App />, document.body);
 
