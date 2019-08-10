22d03ba4a2766deb39444be7a7f3fb41c26e8af4
Antoine du HAMEL <duhamelantoine1995@gmail.com>
2019-08-09 21:59:48 -0400

    Use PureComponents when possible

--- src/components/FeatureStoriesNav.js
+++ src/components/FeatureStoriesNav.js
@@ -2,37 +2,38 @@ import { h, Component } from "preact";
 import { FontAwesomeIcon } from "@aduh95/preact-fontawesome";
 import { faFolderPlus } from "@fortawesome/free-solid-svg-icons";
 
-export default class FeatureStoriesNav extends Component {
-  render() {
-    const { sections } = this.props;
-    return (
-      <nav class="newsletter" data-ignore>
-        {sections.map(section => (
-          <a
-            href={"#" + section.id}
-            data-link-to-edit
-            onClick={this.props.editSection(section)}
-          >
-            <img
-              src={section.illustration}
-              alt={section.illustrationDescription}
-            />
-          </a>
-        ))}
+export default function FeatureStoriesNav({
+  addSection,
+  editSection,
+  sections,
+}) {
+  return (
+    <nav class="newsletter" data-ignore>
+      {sections.map(section => (
         <a
-          href="#"
-          style={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            color: "black",
-          }}
-          data-link-to-create
-          onClick={this.props.addSection}
+          href={"#" + section.id}
+          data-link-to-edit
+          onClick={editSection(section)}
         >
-          <FontAwesomeIcon icon={faFolderPlus} size="2x" />
+          <img
+            src={section.illustration}
+            alt={section.illustrationDescription}
+          />
         </a>
-      </nav>
-    );
-  }
+      ))}
+      <a
+        href="#"
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          color: "black",
+        }}
+        data-link-to-create
+        onClick={addSection}
+      >
+        <FontAwesomeIcon icon={faFolderPlus} size="2x" />
+      </a>
+    </nav>
+  );
 }
--- src/edit_components/lazy-edit-component.js
+++ src/edit_components/lazy-edit-component.js
@@ -1,22 +1,18 @@
-import { h, Component, Fragment } from "preact";
+import { h } from "preact";
 import { Suspense, lazy } from "preact/compat";
 
-export default class EditComponent extends Component {
-  render() {
-    const EditComponent = this.props.active
-      ? lazy(() => import(`./${this.props.componentName}.js`))
-      : Fragment;
+export default function EditComponent(props) {
+  const EditComponent = lazy(() => import(`./${props.componentName}.js`));
 
-    return (
-      <Suspense
-        fallback={
-          <dialog open data-ignore>
-            Loading...
-          </dialog>
-        }
-      >
-        <EditComponent {...this.props.props} />
-      </Suspense>
-    );
-  }
+  return (
+    <Suspense
+      fallback={
+        <dialog open data-ignore>
+          Loading...
+        </dialog>
+      }
+    >
+      {props.active ? <EditComponent {...props.props} /> : null}
+    </Suspense>
+  );
 }
