4016471303d35fe00cda3fce1a56087472d9a8d0
Antoine du HAMEL <duhamelantoine1995@gmail.com>
2019-08-11 22:18:55 -0400

    Add polyfill for Element.prototype.scrollIntoViewIfNeeded

--- src/polyfill/bootstrap.js
+++ src/polyfill/bootstrap.js
@@ -5,3 +5,7 @@ if (!("requestIdleCallback" in window)) {
 if (!("text" in Blob.prototype)) {
   import("./blob-text.js");
 }
+
+if (!("scrollIntoViewIfNeeded" in Element.prototype)) {
+  import("./element-scrollintoviewifneeded.js");
+}
