8bbb13da4a96e0b291a593d14384e7fc20ffce52
Antoine du HAMEL <duhamelantoine1995@gmail.com>
2019-08-14 14:29:59 -0400

    DropZone: use the new state handling

--- src/DropZone.js
+++ src/DropZone.js
@@ -4,6 +4,11 @@ import {
   removeGlobalDragOverListener,
 } from "./createDragHandler.js";
 
+import templateName from "./templateName.js";
+import templateHostName from "./templateHostName.js";
+import templateComponents from "./templateComponents.js";
+import templateCustomCSS from "./TemplateCustomCSS.js";
+
 import "./DropZone.scss";
 
 const DRAG_CLASS_NAME = "dragover";
@@ -17,33 +22,41 @@ const dragEnd = e => {
   e.preventDefault();
   document.documentElement.classList.remove(DRAG_CLASS_NAME);
 };
+
+/**
+ * @param {DragEvent} e
+ */
 const drop = function(e) {
   dragEnd(e);
 
-  const { dataHandler } = this.props;
-
-  for (const file of e.dataTransfer.items || e.dataTransfer.files) {
-    if (file.kind === "file") {
-      file
-        .getAsFile()
-        .text()
-        .then(JSON.parse)
-        .then(dataHandler);
-    } else if (file.kind === "string") {
-      console.warn("File was expected, received string", file);
+  for (const item of e.dataTransfer.items || e.dataTransfer.files) {
+    if (item.kind === "string") {
+      console.warn("File was expected, received string", item);
     } else {
+      /**
+       * @type {File} file
+       */
+      const file = item.kind === "file" ? item.getAsFile() : item;
       file
         .text()
         .then(JSON.parse)
-        .then(dataHandler);
+        .then(({ hostname, css, components }) => {
+          templateName.set(file.name);
+          templateHostName.set(hostname);
+          templateCustomCSS.set(css);
+          templateComponents.set(components);
+        })
+        .catch(e =>
+          import("./notify.js")
+            .then(module => module.default)
+            .then(notify => notify("Cannot import this file", console.error(e)))
+        );
     }
   }
 };
 
 export default class DropZone extends Component {
   state = { previewing: true, content: null };
-
-  #dropHandler = drop.bind(this);
 
   componentDidMount() {
     setGlobalDragOverListener(dragStart);
@@ -59,7 +72,7 @@ export default class DropZone extends Component {
         id={DROP_ZONE_ID}
         onDragLeave={dragEnd}
         onClick={dragEnd}
-        onDrop={this.#dropHandler}
+        onDrop={drop}
       />
     );
   }
