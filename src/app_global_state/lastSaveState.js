80e3ead862451f23e183ba1ace7b8442f7ebd89f
Antoine du HAMEL <duhamelantoine1995@gmail.com>
2019-08-14 21:58:51 -0400

    lastSavedState: implementing missing methods

--- src/app_global_state/lastSaveState.js
+++ src/app_global_state/lastSaveState.js
@@ -1,4 +1,8 @@
-import { LAST_SAVE_KEY } from "./StatePersistance-const.js";
+import {
+  SAVED_TEMPLATE_NAME,
+  PERSISTANT_STORAGE_KEY,
+  LAST_SAVE_KEY,
+} from "./StatePersistance-const.js";
 
 export function lastSaveDate() {
   const previousTimestamp = localStorage.getItem(LAST_SAVE_KEY);
@@ -6,13 +10,18 @@ export function lastSaveDate() {
 }
 
 export function clear() {
-  return import("../notify.js")
-    .then(module => module.default)
-    .then(notify => notify("Not implemented yet"));
+  localStorage.removeItem(SAVED_TEMPLATE_NAME);
+  localStorage.removeItem(PERSISTANT_STORAGE_KEY);
+  localStorage.removeItem(LAST_SAVE_KEY);
 }
 
 export function recover() {
-  return import("../notify.js")
+  return import("./initiateState.js")
     .then(module => module.default)
-    .then(notify => notify("Not implemented yet"));
+    .then(initiateState =>
+      initiateState.initiateWith({
+        name: localStorage.getItem(SAVED_TEMPLATE_NAME),
+        ...JSON.parse(localStorage.getItem(PERSISTANT_STORAGE_KEY)),
+      })
+    );
 }
