9d1b029aa5b9eae5504477ff08344149a9d27309
Antoine du HAMEL <duhamelantoine1995@gmail.com>
2019-08-14 21:08:43 -0400

    AppError: update to the new state handling

--- src/AppError.js
+++ src/AppError.js
@@ -1,18 +1,32 @@
 import { h } from "preact";
-import statePersistance from "./StatePersistance.js";
 
 import "./AppError.css";
+
+const historyAction = action =>
+  import("./app_global_state/History.js")
+    .then(module => module.default)
+    .then(action)
+    .catch(() =>
+      import("./notify.js")
+        .then(module => module.default)
+        .then(notify => notify("Cannot undo last action"))
+        .then(() => Promise.reject(e))
+    );
 
 export default function Error(props) {
   const cancelLastAction = e => {
     e.preventDefault();
-    statePersistance.rewindToPreviousState();
-    props.resetState();
+    historyAction(history => history.rewindToPreviousState()).then(
+      () => props.resetState(),
+      console.error
+    );
   };
   const resetApp = e => {
     e.preventDefault();
-    statePersistance.clearCurrentSession();
-    props.resetState();
+    historyAction(history => history.clearCurrentSession()).then(
+      () => props.resetState(),
+      console.error
+    );
   };
   console.log("render");
   return (
