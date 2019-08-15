62c86660b219d233628edf3d23a89ea974d845a8
Antoine du HAMEL <duhamelantoine1995@gmail.com>
2019-08-14 21:57:07 -0400

    AppError: fix crash

--- src/AppError.js
+++ src/AppError.js
@@ -2,42 +2,40 @@ import { h } from "preact";
 
 import "./AppError.css";
 
-const historyAction = action =>
+const historyAction = (action, errorMessage) =>
   import("./app_global_state/History.js")
     .then(module => module.default)
     .then(action)
     .catch(() =>
       import("./notify.js")
         .then(module => module.default)
-        .then(notify => notify("Cannot undo last action"))
+        .then(notify => notify(errorMessage))
         .then(() => Promise.reject(e))
     );
 
 export default function Error(props) {
   const cancelLastAction = e => {
     e.preventDefault();
-    historyAction(history => history.rewindToPreviousState()).then(
-      () => props.resetState(),
-      console.error
-    );
+    historyAction(
+      history => history.rewindToPreviousState(),
+      "Cannot undo last action"
+    ).then(() => props.resetState(), console.error);
   };
   const resetApp = e => {
     e.preventDefault();
-    historyAction(history => history.clearCurrentSession()).then(
-      () => props.resetState(),
-      console.error
-    );
+    historyAction(
+      history => history.clearCurrentSession(),
+      "Cannot reset current session"
+    ).then(() => props.resetState(), console.error);
   };
   console.log("render");
   return (
     <main data-ignore className="error">
       <h2>Oops!</h2>
       <p>You have encountered an error.</p>
-      {statePersistance.hasPreviousState ? (
-        <a onClick={cancelLastAction} href="#">
-          Cancel last action
-        </a>
-      ) : null}
+      <a onClick={cancelLastAction} href="#">
+        Cancel last action
+      </a>
       <a onClick={resetApp} href="#">
         Reset the app
       </a>
