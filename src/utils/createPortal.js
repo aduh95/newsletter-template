ab4a34391f0988da514e1a9213e8ecba1f5dcf1d
Antoine du HAMEL <duhamelantoine1995@gmail.com>
2019-09-10 21:30:45 -0400

    Fix issue on createPortal

--- src/utils/createPortal.js
+++ src/utils/createPortal.js
@@ -1,15 +1,38 @@
+import "./createPortal.css";
+
 const PORTAL_ELEMENT_NAME = "dom-portal";
 
 class Portal extends HTMLElement {
   #domElement;
+  #parent;
 
   attach(element, parent) {
     this.#domElement = parent.appendChild(element);
+    this.#domElement.append(...this.children);
+    this.#parent = parent;
   }
 
-  remove() {
-    super.remove();
-    this.#domElement.remove();
+  append(...children) {
+    return this.#domElement
+      ? this.#domElement.append(...children)
+      : super.append(...children);
+  }
+  appendChild(child) {
+    return this.#domElement
+      ? this.#domElement.appendChild(child)
+      : super.appendChild(child);
+  }
+
+  connectedCallback() {
+    if (this.#parent && this.#domElement && this.isConnected) {
+      this.#parent.append(this.#domElement);
+    }
+  }
+
+  disconnectedCallback() {
+    if (this.#domElement?.isConnected) {
+      this.#domElement.remove();
+    }
   }
 }
 
@@ -18,13 +41,17 @@ customElements.define(PORTAL_ELEMENT_NAME, Portal);
 /**
  * @param {Promise<Node>} asyncElement Async element
  * @param {ParentNode|Promise<ParentNode>} parent Node to be displayed until the element settle
+ * @param {Function} callback
  * @return {ChildNode}
  */
-export function createPortal(asyncElement, parent) {
+export function createPortal(asyncElement, parent, callback = null) {
   const portal = document.createElement(PORTAL_ELEMENT_NAME);
 
   Promise.resolve(asyncElement).then(element => {
     portal.attach(element, parent);
+    if ("function" === typeof callback) {
+      requestAnimationFrame(callback);
+    }
   });
 
   return portal;
