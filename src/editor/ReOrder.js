82cd50789f98968d5e58fb9c8fd21479140e125c
Antoine du HAMEL <duhamelantoine1995@gmail.com>
2019-10-18 11:24:07 +0100

    ReOrder: restore scroll position after reordering

--- src/editor/ReOrder.js
+++ src/editor/ReOrder.js
@@ -4,15 +4,25 @@ import { FontAwesomeIcon } from "@aduh95/preact-fontawesome";
 import { faSort } from "@fortawesome/free-solid-svg-icons";
 
 export default class ReOrder extends PureComponent {
+  #savedScrollPosition;
+  #requestScroll = false;
   #toggleReOrder = () =>
-    this.props.editor.setState(state => {
-      if (state.reOrder) {
-        this.props.editor.commitChanges();
-        return { reOrder: false };
-      } else {
-        return { reOrder: true };
-      }
-    });
+    this.props.editor.setState(
+      state => {
+        if (state.reOrder) {
+          this.props.editor.commitChanges();
+          this.#requestScroll = true;
+          return { reOrder: false };
+        } else {
+          this.#savedScrollPosition = window.scrollY;
+          return { reOrder: true };
+        }
+      },
+      () =>
+        this.#requestScroll &&
+        !(this.#requestScroll = false) &&
+        window.scrollTo(0, this.#savedScrollPosition)
+    );
 
   render() {
     console.log("render");
