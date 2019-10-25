1f45f8cb20b7c0a631ffe5d025990890cbc11122
Antoine du HAMEL <duhamelantoine1995@gmail.com>
2019-10-25 19:30:42 -0400

    HotTopics: improve keyboard navigation

--- src/components/HotTopics.js
+++ src/components/HotTopics.js
@@ -28,10 +28,12 @@ export default class HotTopics extends Component {
 
   componentDidUpdate() {
     if (this.#readyToCleanState) {
-      this.setState(
-        { newTopic: null },
-        () => (this.#readyToCleanState = false)
-      );
+      this.setState({ newArticle: null }, () => {
+        this.#readyToCleanState = false;
+        requestAnimationFrame(() =>
+          this.base?.querySelector("article:last-of-type")?.focus()
+        );
+      });
     } else if (this.#readyToConsumeState) {
       this.#readyToCleanState = true;
       this.#readyToConsumeState = false;
