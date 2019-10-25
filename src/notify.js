8dd0ce1c493e25b9bd591da9344fb06a2eb7a390
Antoine du HAMEL <duhamelantoine1995@gmail.com>
2019-10-25 14:39:22 -0400

    Notify: improve accessibility

--- src/notify.js
+++ src/notify.js
@@ -1,18 +1,30 @@
 import "./notify.css";
+const NOTIFICATION_ELEMENT_NAME = "inline-notification";
+class NotificationElement extends HTMLElement {
+  constructor() {
+    super();
+
+    this.addEventListener("click", Element.prototype.remove);
+  }
+
+  connectedCallback() {
+    if (this.isConnected) {
+      setTimeout(() => {
+        this.animate([{ opacity: 1 }, { opacity: 0 }], {
+          duration: 3000,
+        }).onfinish = () => this.remove();
+      }, 5000);
+    }
+  }
+}
+
+customElements.define(NOTIFICATION_ELEMENT_NAME, NotificationElement);
 
 export default function notify(message) {
-  const notif = document.createElement("div");
-  const removeNotif = () => notif.remove();
+  const notification = document.createElement(NOTIFICATION_ELEMENT_NAME);
 
-  notif.className = "notification";
-  notif.appendChild(document.createTextNode(message));
+  notification.setAttribute("title", message);
+  notification.setAttribute("role", "alert");
 
-  document.body.append(notif);
-
-  notif.addEventListener("click", removeNotif);
-  setTimeout(() => {
-    notif.animate([{ opacity: 1 }, { opacity: 0 }], {
-      duration: 3000,
-    }).onfinish = removeNotif;
-  }, 5000);
+  document.body.append(notification);
 }
