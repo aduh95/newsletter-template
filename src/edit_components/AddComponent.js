cd5ee0da37a02d6bce28469e2c785e05029ff54e
Antoine du HAMEL <duhamelantoine1995@gmail.com>
2019-10-25 19:58:45 -0400

    AddComponent: improve keyboard navigation

--- src/edit_components/AddComponent.js
+++ src/edit_components/AddComponent.js
@@ -1,8 +1,10 @@
-import { h, Fragment } from "preact";
+import { h, Fragment, createRef } from "preact";
 import { PureComponent } from "preact/compat";
 
 export default class AddNewComponent extends PureComponent {
   state = { newComponent: null };
+
+  #selectRef = createRef();
 
   #handleChange = this.handleChange.bind(this);
 
@@ -25,7 +27,11 @@ export default class AddNewComponent extends PureComponent {
           newComponent: (
             <EditComponent
               type={value}
-              resetState={() => this.setState({ newComponent: null })}
+              resetState={() =>
+                this.setState({ newComponent: null }, () =>
+                  requestAnimationFrame(() => this.#selectRef.current?.focus())
+                )
+              }
               saveState={this.addNewComponent.bind(this)}
             />
           ),
@@ -42,6 +48,7 @@ export default class AddNewComponent extends PureComponent {
           data-ignore
           data-do-not-export
           onChange={this.#handleChange}
+          ref={this.#selectRef}
           style="width:100%"
         >
           <option>Add new component</option>
